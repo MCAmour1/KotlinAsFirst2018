e6708d14c8b8841f756ad7f69e8d430dcc95cbb9
Svyatoslav <svat.deve@gmail.com>
2020-04-13 03:31:00 +0300

    First Chapter Read and Write Second Chapter Read 50%

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -1,8 +1,11 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sqrt
 
 /**
@@ -62,7 +65,30 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+
+
+fun lastNumber(age: Int): Int {
+    if (age < 0) return -1 //отправлю код ошибки "Меньше нуля. За рамками задачи"
+    else if (age in 0..9) return age
+    else if (age in 10..99) return age % 10
+    else if (age in 100..200) return (age - age / 100 * 100) % 10
+    else return -200 //отправлю код ошибки "Больше 200. За рамками задачи"
+}
+
+fun ageDescription(age: Int): String {
+    /* В начале найдем последнюю цифру в числе */
+    val lastNumber: Int = lastNumber(age)
+    /* Вторым шагом вывод соответствующего литерала */
+    if (age in 11..14 || age in 111..114) {
+        return "$age лет"
+    } else if (lastNumber == 1) {
+        return "$age год"
+    } else if (lastNumber in 2..4) {
+        return "$age года"
+    } else {
+        return "$age лет"
+    }
+}
 
 /**
  * Простая
@@ -73,7 +99,27 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val s1 = t1 * v1
+    val s2 = t2 * v2
+    val s3 = t3 * v3
+    val s = s1 + s2 + s3
+    println("Всё расстояние: $s = $s1 + $s2 + $s3")
+    val half_s = s / 2
+    println("Половина расстояния: $half_s")
+
+    if (half_s < s1) return half_s / v1
+    else if (half_s < (s1 + s2)) {
+        println("Т.к. половина расстояния меньше второго отрезка и больше первого отрезка: ")
+        val part_s2 = half_s - s1
+        val part_t2 = part_s2 / v2
+        println("Кусочек второго отрезка: $part_s2 за $part_t2 времени")
+        val half_t = part_t2 + t1
+        println("Результат: $half_t")
+        return half_t
+    } else if (half_s < s) return (half_s - s1 - s2) / v3 + t2 + t1
+    else return s3 / v3 + s2 / v2 + s1 / v1
+}
 
 /**
  * Простая
@@ -86,8 +132,40 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    var result: Int = 0
+    when (kingX) {
+        rookX1 -> result += 1
+        rookX2 -> result += 2
+    }
+    when (kingY) {
+        rookY1 -> result += 1
+        rookY2 -> result += 2
+    }
+    return result
+}
 
+
+
+
+fun diagonalChess(coordinates: Pair<Int, Int>, vector: Pair<Boolean, Boolean>): Array<Pair<Int, Int>> {
+    var diagonal = emptyArray<Pair<Int, Int>>()
+    var x1 = coordinates.first
+    var y1 = coordinates.second
+    if(vector.first==true && vector.second==true) while (x1 < 8 && y1 < 8) diagonal += ++x1 to ++y1
+    if(vector.first==false && vector.second==false) while (x1 > 1 && y1 > 1) diagonal += --x1 to --y1
+    if(vector.first==true && vector.second==false) while (x1 < 8 && y1 > 1) diagonal += ++x1 to --y1
+    if(vector.first==false && vector.second==true) while (x1 > 1 && y1 < 8) diagonal += --x1 to ++y1
+    return diagonal
+}
+fun allDiagonalChess(coordinates: Pair<Int, Int>): Array<Pair<Int, Int>> {
+    val diagonalMax = diagonalChess(coordinates, (true to true))
+    val diagonalMin = diagonalChess(coordinates, (false to false))
+    val diagonalOnlyXMax = diagonalChess(coordinates, (true to false))
+    val diagonalOnlyYMax = diagonalChess(coordinates, (false to true))
+    val allDiagonal = diagonalMax + diagonalMin + diagonalOnlyXMax + diagonalOnlyYMax
+    return allDiagonal
+}
 /**
  * Простая
  *
@@ -98,10 +176,39 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  * и 3, если угроза есть и от ладьи и от слона.
  * Считать, что ладья и слон не могут загораживать друг друга.
  */
+
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int {
+    var result = 0
+    val kingCoordinates = kingX to kingY
+    val bishopCoordinates = bishopX to bishopY
+    val allDiagonal = allDiagonalChess(kingCoordinates)
+    if(allDiagonal.contains(bishopCoordinates)) result += 2
+    if(kingX == rookX || kingY == rookY) result += 1
+    return result
+}
 
+
+fun main(args: Array<String>) {
+    val a = 5.0
+    val b = 4.0
+    val c = 4.0
+
+}
+fun thCosAngle(max: Double, b: Double, c: Double): Double =
+        (sqr(max) - sqr(b) - sqr(c)) / (-2 * b * c)
+
+fun triangleKindCos(a: Double, b: Double, c: Double): Double {
+    /* Сравним стороны */
+    val m = max(max(a, b), c)
+    when (m){
+        a -> return thCosAngle(a, b, c)
+        b -> return thCosAngle(b, a, c)
+        c -> return thCosAngle(c, a, b)
+        else -> return -1.0
+    }
+}
 /**
  * Простая
  *
@@ -110,7 +217,16 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val cos = triangleKindCos(a,b,c)
+    when{
+        (a>b+c || b>a+c || c>a+b) -> return -1
+        cos > 0 -> return 0
+        cos == 0.0 -> return 1
+        else -> return 2
+    }
+    println(cos)
+}
 
 /**
  * Средняя
@@ -121,3 +237,14 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Если пересечения нет, вернуть -1.
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+//    val begin_first = min(a,c)
+//    val end_first = max(a,c)
+//
+//    val end_last = max(b,d)
+//    val begin_last = min(b,d)
+//    if (begin_first == a) if ()
+//    }
+//
+//    if (begin_first == end_last) return 1
+//    else return -1
+//}
